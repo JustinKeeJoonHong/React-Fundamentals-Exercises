c05eeec3e8b7bad3b59363352789269a2323c93a
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-03-21 14:16:15 -0700

    set app()

--- L3. State Management/2. Passing Data II/Starter/src/App.js
+++ L3. State Management/2. Passing Data II/Starter/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Dashboard from "./Dashboard";
+import MovieCard from "./MovieCard";
 import logo from "./logo.svg";
 
 // Display a list of movies where each movie contains a list of users that favorited it.
@@ -94,6 +96,17 @@ const movies = {
 };
 
 const App = () => {
+  let usersByMovie = {};
+
+  profiles.forEach((profile) => {
+    const movieId = profile.favoriteMovieID;
+
+    if (usersByMovie[movieId]) {
+      usersByMovie[movieID].push(profile.userID);
+    } else {
+      usersByMovie[movieID] = [profile.userID];
+    }
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -101,6 +114,11 @@ const App = () => {
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <h2>How Popular is Your Favorite Movie?</h2>
+      <Dashboard
+        usersByMovie={usersByMovie}
+        movies={movies}
+        users={users}
+      ></Dashboard>
     </div>
   );
 };
--- L3. State Management/2. Passing Data II/Starter/src/MovieCard.js
+++ L3. State Management/2. Passing Data II/Starter/src/MovieCard.js
@@ -1,11 +1,20 @@
-const MovieCard = (movies) => {
+const MovieCard = (props) => {
+  console.log(props);
   return (
     <oi>
-      {movies.map((movie) => {
-        <h2>{movie.name}</h2>;
-        <p>
-          {True ? "Liked By:" : "None of the current users liked this movie"}
-        </p>;
+      {Object.keys(props.movies).map((movieId) => {
+        const movie = props.moives[movieId];
+        console.log(movieId);
+        return (
+          <li key={movie.id}>
+            <h2>{movie.name}</h2>
+            <p>
+              {true
+                ? "Liked By:"
+                : "None of the current users liked this movie"}
+            </p>
+          </li>
+        );
       })}
     </oi>
   );
