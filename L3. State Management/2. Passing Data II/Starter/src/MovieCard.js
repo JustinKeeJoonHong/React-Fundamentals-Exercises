3880cb6e6dff02e8dbdcc6eefa183c0fd33bbf9c
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-03-21 15:38:16 -0700

    complete MovieCard.js except represent UserList

--- L3. State Management/2. Passing Data II/Starter/src/MovieCard.js
+++ L3. State Management/2. Passing Data II/Starter/src/MovieCard.js
@@ -1,25 +1,15 @@
 const MovieCard = ({ users, usersWhoLikedMovie, movieInfo }) => {
-  console.log("this is userswhoLikedMovie");
-  console.log(usersWhoLikedMovie);
-  console.log(typeof usersWhoLikedMovie);
-  console.log("below is movieInfo");
-  console.log(movieInfo);
-  console.log(typeof movieInfo);
+  const likedByMessage = usersWhoLikedMovie
+    ? usersWhoLikedMovie.length !== 0
+      ? "Liked By:"
+      : "None of the current users liked this movie"
+    : "None of the current users liked this movie";
+
   return (
-    <oi>
-      {Object.keys(movieInfo).map((movieId) => {
-        return (
-          <li key={movieId.id}>
-            <h2>{movieId.name}</h2>
-            <p>
-              {usersWhoLikedMovie.length == 0
-                ? "Liked By:"
-                : "None of the current users liked this movie"}
-            </p>
-          </li>
-        );
-      })}
-    </oi>
+    <div>
+      <h2 key={movieInfo.id}>{movieInfo.name}</h2>
+      <p>{likedByMessage}</p>
+    </div>
   );
 };
 
