7f786cf3a265e07af570ab56d506814ad45ec50f
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-03-22 05:36:25 -0700

    complete L3/3.Managing Starter

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import corgi from "./corgi.jpg";
 import "./App.css";
 
 const App = () => {
+  const [likedNum, setLikeNum] = useState(0);
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <p>Amount of likes: {likedNum}</p>
+      <button onClick={() => setLikeNum(likedNum + 1)}>Like</button>
     </div>
   );
 };
