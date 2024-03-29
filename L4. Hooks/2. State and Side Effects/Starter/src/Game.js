98248d36a0bb904ffab33d61c75c271fdd3cbda8
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-03-28 17:30:39 -0700

    add makeNewQuestion

--- L4. Hooks/2. State and Side Effects/Starter/src/Game.js
+++ L4. Hooks/2. State and Side Effects/Starter/src/Game.js
@@ -1,11 +1,22 @@
 import Score from "./Score";
 
-const Game = (props) => {
+const Game = ({ handleAnswer }) => {
   const [value1, setValue1] = useState(null);
   const [value2, setValue2] = useState(null);
   const [value3, setValue3] = useState(null);
   const [proposedAnswer, setProposedAnswer] = useState(null);
-  const checkTrue = () => {};
+
+  const makeNewQuestion = () => {
+    const value1 = Math.floor(Math.random() * 100);
+    const value2 = Math.floor(Math.random() * 100);
+    const value3 = Math.floor(Math.random() * 100);
+
+    const proposedAnswer =
+      Math.floor(Math.random() * 3) + (value1 + value2 + value3);
+
+    return [value1, value2, value3, proposedAnswer];
+  };
+
   return (
     <div>
       <div className="equation">
