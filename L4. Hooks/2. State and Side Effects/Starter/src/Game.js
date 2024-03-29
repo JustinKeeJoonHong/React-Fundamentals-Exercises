f37fe745eef396100de75de9f9b5e372f6c6ac7f
JustinKeeJoonHong <105581488+JustinKeeJoonHong@users.noreply.github.com>
2024-03-28 18:47:59 -0700

    complete Game.js

--- L4. Hooks/2. State and Side Effects/Starter/src/Game.js
+++ L4. Hooks/2. State and Side Effects/Starter/src/Game.js
@@ -1,10 +1,19 @@
 import Score from "./Score";
+import { useState, useEffect } from "react";
 
 const Game = ({ handleAnswer }) => {
   const [value1, setValue1] = useState(null);
   const [value2, setValue2] = useState(null);
   const [value3, setValue3] = useState(null);
   const [proposedAnswer, setProposedAnswer] = useState(null);
+
+  useEffect(() => {
+    let valuesArray = makeNewQuestion();
+    setValue1(valuesArray[0]);
+    setValue2(valuesArray[1]);
+    setValue3(valuesArray[2]);
+    setProposedAnswer(valuesArray[3]);
+  }, []);
 
   const makeNewQuestion = () => {
     const value1 = Math.floor(Math.random() * 100);
@@ -17,13 +26,40 @@ const Game = ({ handleAnswer }) => {
     return [value1, value2, value3, proposedAnswer];
   };
 
+  const refresh = (newValuesArray) => {
+    setValue1(newValuesArray[0]);
+    setValue2(newValuesArray[1]);
+    setValue3(newValuesArray[2]);
+    setProposedAnswer(newValuesArray[3]);
+  };
+
+  const handleAnswer = (event) => {
+    const newValuesArray = makeNewQuestion();
+    refresh(newValuesArray);
+    const answerWasCorrect = evaluateAnswer(event.target.name);
+    props.handleAnswer(answerWasCorrect);
+  };
+
+  const evaluateAnswer = (givenAnswer) => {
+    const corrAnswer = value1 + value2 + value3;
+
+    return (
+      (corrAnswer === proposedAnswer && givenAnswer === "true") ||
+      (corrAnswer !== proposedAnswer && givenAnswer === "false")
+    );
+  };
+
   return (
     <div>
       <div className="equation">
         <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
       </div>
-      <button>True</button>
-      <button>False</button>
+      <button onClick={handleAnswer} name="true">
+        True
+      </button>
+      <button onClick={handleAnswer} name="false">
+        False
+      </button>
     </div>
   );
 };
